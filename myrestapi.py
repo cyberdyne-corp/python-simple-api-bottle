3c9914d90056478e5196ada29940f5be2cda55d7
Anthony Lapenna <lapenna.anthony@gmail.com>
2015-04-18 16:36:06 +0200

    Added ContainerManager object.

--- myrestapi.py
+++ myrestapi.py
@@ -1,22 +1,35 @@
 from bottle import route, run, request, abort
 
 
+class ContainerManager:
+
+    def list_containers():
+        print('Listing containers')
+
+    def create_container(data):
+        print('Creating container using the following data: %s' % data)
+
+    def delete_container(name):
+        print('Killing container %s.' % name)
+
+manager = ContainerManager
+
+
 @route('/containers/', method='GET')
-def list_containers():
-    print('Listing containers.')
+def list():
+    manager.list_containers()
 
 
-@route('/containers', method='POST')
-def create_container():
+@route('/containers/', method='POST')
+def create():
     data = request.json
     if not data:
         abort(400, 'No data received')
-    print ('Creating container using the following data: %s' % data)
+    manager.create_container(data)
 
 
 @route('/containers/:name', method='DELETE')
-def delete_container(name):
-    print('Killing container %s.' % name)
-
+def delete(name):
+    manager.delete_container(name)
 
 run(host='localhost', port=8080)
